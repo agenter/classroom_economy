a605bd7d0ca1a34e3da5ea1b6a876a36b78afbd8
Aaron Genter <agenter@lightspeedsystems.com>
2013-02-17 12:15:37 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassroomEconomy::Application.config.secret_token = '36ad3735f93170950bb52d9e2dd0732bd388a2e81fabb758739d7788cceb19016e8d7970feceb9a112458c00ea713afe86c9e9a6e616e289b7195faa8180f01d'
